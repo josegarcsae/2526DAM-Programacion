f46c25e9d62f3c8269c9d0e03ecf71ca7ad07afb
Guzman <joseg.arcsae@educa.jcyl.es>
2026-03-09 20:33:17 +0100

    T5: listas

---
+++ src/main/java/Tema5/ej2Videoclub/Prestamo.java
@@ -0,0 +1,32 @@
+package Tema5.ej2Videoclub;
+
+import java.time.LocalDate;
+
+public class Prestamo {
+    private Pelicula pelicula;
+    private Cliente cliente;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucion;
+
+    public Prestamo(Pelicula pelicula, Cliente cliente, LocalDate fechaPrestamo) {
+        this.pelicula = pelicula;
+        this.cliente = cliente;
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+}
